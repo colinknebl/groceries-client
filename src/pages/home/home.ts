ed6e3350d0fd060527b6ee866a15e067c8d197c3
Colin Knebl <colinknebl@outlook.com>
2020-03-11 22:02:40 -0400

    commit week 2 work

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,56 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
 
-@Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
-})
-export class HomePage {
+class GroceryItem {
+  private _name: string;
+  private _quantity: number;
+  private _price: number;
+  private _priceFormatter: Intl.NumberFormat;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(
+    name: string,
+    quantity: number,
+    price: number,
+    priceFormatter: Intl.NumberFormat
+  ) {
+    this._name = name;
+    this._quantity = quantity;
+    this._price = price;
+    this._priceFormatter = priceFormatter;
   }
 
+  get name(): string {
+    return this._name;
+  }
+
+  get quantity(): number {
+    return this._quantity;
+  }
+
+  get formattedPrice(): string {
+    return this._priceFormatter.format(this._price);
+  }
+
+  get formattedTotal(): string {
+    const total = this._price * this._quantity;
+    return this._priceFormatter.format(total);
+  }
+}
+
+@Component({
+  selector: "page-home",
+  templateUrl: "home.html"
+})
+export class HomePage {
+  private _priceFormatter: Intl.NumberFormat = new Intl.NumberFormat("en-us", {
+    style: "currency",
+    currency: "USD"
+  });
+  public groceries: GroceryItem[] = [
+    new GroceryItem("Milk", 2, 2.99, this._priceFormatter),
+    new GroceryItem("Bread", 4, 1.99, this._priceFormatter),
+    new GroceryItem("Frozen Pizza", 1, 5.99, this._priceFormatter)
+  ];
+
+  constructor(public navCtrl: NavController) {}
 }
